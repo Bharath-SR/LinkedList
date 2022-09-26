7427867ca54132372e414f83aee2cf130005a6bb
Bharath-SR <bharath.s0941@gmail.com>
2022-09-26 06:50:52 +0530

    UC4:Added center element

--- LinkedList/src/com/bridgeLabz/LinkedList.java
+++ LinkedList/src/com/bridgeLabz/LinkedList.java
@@ -24,7 +24,18 @@ public class LinkedList {
 
 		}
 	}
+	public void pushFirst(Object data) {
+		Node newNode = new Node(data);
 
+		if (head == null)
+			head = newNode;
+		else {
+			Node temp = head;
+			head = newNode;
+			head.next = temp;
+
+		}
+	}
 	public void pushLast(Object data) {
 		Node newNode = new Node(data);
 		if (head == null)
@@ -38,24 +49,74 @@ public class LinkedList {
 		}
 	}
 	public void display() {
-
-		if (head == null) {
-			System.out.println("List is empty");
-		}
-		Node temp = head;
-		while (temp != null) {
-			System.out.println(temp.data);
-			temp = temp.next;
+		if (head == null)
+			System.out.println("No elements in list to display...");
+		else if (head.next == null)
+			System.out.println(head.data);
+		else {
+			Node temp = head;
+			while (temp != null) {
+				if (temp.next != null)
+					System.out.print(temp.data + "-->");
+				else
+					System.out.print(temp.data + "\n");
+				temp = temp.next;
+			}
 		}
 	}
 
+	public int size() {
+		int count = 0;
+
+		Node temp = head;
+
+		while (temp != null) {
+			count++;
+			temp = temp.next;
+		}
+
+		return count;
+
+	}
+
+	public void insertIndex(int userIndex, Object data) {
+
+		if (userIndex == 0)
+			pushFirst(data);
+		else if (userIndex == size())
+			pushLast(data);
+
+		else if (userIndex < 0 || userIndex >= size())
+			System.out.println("Invalid index");
+
+
+		else {
+		
+			Node newNode = new Node(data);
+
+
+			int index = 0;
+			Node left = head;
+			Node right = left.next;
+
+			while (index < userIndex - 1) {
+				left = left.next;
+				right = right.next;
+				index++;
+			}
+			newNode.next = right;
+			left.next = newNode;
+		}
+	}
+
+
 	public static void main(String[] args) {
-		//Driving Class
-		System.out.println("Linked List Program:");
+		// Driving Class
+		System.out.println("Linked List Program");
 		LinkedList input = new LinkedList();
-		input.pushLast(70);
-		input.pushLast(30);
 		input.pushLast(56);
+		input.pushLast(70);
+		input.insertIndex(1,30);
 		input.display();
 		
 	}
