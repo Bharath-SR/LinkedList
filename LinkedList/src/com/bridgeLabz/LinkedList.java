c030668d583939aa608688bd2c02ac7bbf9b2c19
Bharath-SR <bharath.s0941@gmail.com>
2022-09-26 06:32:31 +0530

    Started: Linked List Program

---
+++ LinkedList/src/com/bridgeLabz/LinkedList.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author Admin
+ *
+ */
+public class LinkedList {
+
+	/**
+	 * 
+	 * Linked List Program Master
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
