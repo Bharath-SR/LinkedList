8ce2738c1520f8ddd07a928e9e2269e605e1efb3
Bharath-SR <bharath.s0941@gmail.com>
2022-09-26 06:37:54 +0530

    UC1: Added Values 56,30,70 in linked list

--- LinkedList/src/com/bridgeLabz/LinkedList.java
+++ LinkedList/src/com/bridgeLabz/LinkedList.java
@@ -11,11 +11,59 @@ public class LinkedList {
 
 	/**
 	 * 
-	 * Linked List Program Master
+	 *UC1-Creating Linked List
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public Node head;
 
+	public class Node { 		
+
+		Object data;	
+		Node next;		
+
+		public Node(Object data) {		 
+			this.data = data;
+
+		}
 	}
+	public void push(int data) {
+
+		Node newNode = new Node(data);		
+
+		if (head == null) {
+			head = newNode;
+
+		} else {
+			Node temp = head;		
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+
+		}
+	}
+
+	public void display() {
+		if (head == null) {
+			System.out.println("List is empty");
+		}
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		//Driving Class
+		System.out.println("Linked List has been created: ");
+		LinkedList input = new LinkedList();
+		input.push(56);
+		input.push(30);
+		input.push(70);
+		input.display();
+	}
+
+
 
 }
